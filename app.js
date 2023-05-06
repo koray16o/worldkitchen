d0b322036423f44435c071977f0ecfc5cc4a00ca
Koray Ojeda <korayatlanojeda@gmail.com>
2023-05-06 14:12:00 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,77 @@
+// ℹ️ Gets access to environment variables/settings
+// https://www.npmjs.com/package/dotenv
+require('dotenv').config();
+
+// ℹ️ Connects to the database
+require('./db');
+
+// Handles http requests (express is node js framework)
+// https://www.npmjs.com/package/express
+const express = require('express');
+
+// Handles the handlebars
+// https://www.npmjs.com/package/hbs
+const hbs = require('hbs');
+
+// Added helpers because I want to use
+// the #eq helper
+const helpers = require('handlebars-helpers');
+hbs.registerHelper(helpers());
+
+const app = express();
+
+// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
+require('./config')(app);
+
+const session = require('express-session');
+const mongoStore = require('connect-mongo');
+app.use(
+  session({
+    resave: true,
+    saveUninitialized: true,
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+      sameSite: true, //front end and backend are running on localhost 3000 so it is true
+      httpOnly: true, //we are not using https
+      maxAge: 60000 //Session time in miliseconds (here is 1 min)
+    },
+    rolling: true,
+    store: new mongoStore({
+      mongoUrl: process.env.MONGODB_URI,
+      ttl: 60 * 60 * 24 //We will store the users data (cookie) 1 day in the database
+    })
+  })
+);
+//Custom middleware to get the current logged user
+function getCurrentLoggedUser(req, res, next) {
+  if (req.session && req.session.currentUser) {
+    app.locals.currentUser = req.session.currentUser;
+  } else {
+    app.locals.currentUser = '';
+  }
+  next();
+}
+
+//Use the middleware
+app.use(getCurrentLoggedUser);
+
+// default value for title local
+const capitalize = require('./utils/capitalize');
+const projectName = 'library-project';
+
+app.locals.appTitle = `${capitalize(projectName)} created with IronLauncher`; //If you change the name here, it will also change on thw web
+
+// 👇 Start handling routes here
+const indexRoutes = require('./routes/index.routes');
+app.use('/', indexRoutes);
+
+const bookRoutes = require('./routes/book.routes');
+app.use('/', bookRoutes);
+
+const authRoutes = require('./routes/auth.routes');
+app.use('/', authRoutes);
+
+// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
+require('./error-handling')(app);
+
+module.exports = app;
---
+++ routes/auth.routes.js
@@ -0,0 +1,76 @@
+const router = require('express').Router();
+const User = require('../models/User.model');
+const bcrypt = require('bcryptjs');
+
+router.get('/signup', (req, res) => {
+  res.render('auth/signup');
+});
+
+router.post('/signup', async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (username === '' || email === '' || password === '') {
+    res.render('auth/signup', { errorMessage: 'Fill in all the fields' });
+    return;
+  }
+
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (regex.test(password) === false) {
+    res.render('auth/signup', { errorMessage: 'Password is too weak' });
+    return;
+  }
+
+  const user = await User.findOne({ username });
+  if (user !== null) {
+    res.render('auth/signup', { errorMessage: 'Username already exists' });
+    return;
+  }
+
+  const saltRounds = 10;
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hashedPassword = bcrypt.hashSync(password, salt);
+
+  await User.create({
+    username,
+    email,
+    password: hashedPassword
+  });
+  res.redirect('/');
+});
+
+router.get('/login', (req, res) => {
+  res.render('auth/login');
+});
+
+router.post('/login', async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (username === '' || password === '') {
+    res.render('auth/login', { errorMessage: 'Invalid Login' });
+    return;
+  }
+
+  const user = await User.findOne({ username });
+  if (!user) {
+    res.render('auth/login', { errorMessage: 'Invalid Login' });
+    return;
+  }
+
+  //checking for password
+  if (bcrypt.compareSync(password, user.password)) {
+    //Passwords match
+    req.session.currentUser = user;
+    res.redirect('/');
+  } else {
+    //Passwords dont match
+    res.render('auth/login', { errorMessage: 'Invalid Login' });
+    return;
+  }
+});
+
+router.post('/logout', (req, res) => {
+  req.session.destroy();
+  res.redirect('/');
+});
+
+module.exports = router;
